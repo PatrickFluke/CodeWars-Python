91a11ab64c346177581430229129a35a92d8a6e3
Patrick Fluke <pxfluke@gmail.com>
2020-08-29 08:41:22 -0400

    Update Stop_gninnipS_My_sdroW!.py

--- Stop_gninnipS_My_sdroW!.py
+++ Stop_gninnipS_My_sdroW!.py
@@ -1,3 +1,6 @@
+#I'm very well aware how much longer this is than it had to be. This was just the first method that came to mind. I'm continually practicing
+#to reduce the number of lines of code I require.
+
 def spin_words(sentence):
     # Your code goes here
     split_sentence = sentence.split(sep=" ")
